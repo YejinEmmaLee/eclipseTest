01da4d5c519e78e9c4fc321cf0b75d4b195a687d
YJ <s2nijey@naver.com>
2017-03-28 10:38:19 +0900

    세번째 second

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello GitWorld! ");
 		System.out.println("second branch 에서 작업한 내용");
-
+		System.out.println("second branch에서 또오작업함");
 	}
 
 }
