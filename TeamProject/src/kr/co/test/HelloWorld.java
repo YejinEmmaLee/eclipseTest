72ce3c0252c81bce966b0301e31dcf541b64539a
YJ <s2nijey@naver.com>
2017-03-28 10:13:30 +0900

    세번째 커밋

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello GitWorld! ");
 		System.out.println("second branch 에서 작업한 내용");
+		System.out.println("마스터에서 작업 ~!");
 
 	}
 
